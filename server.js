ef7b59f504ede29bf0dedced551a31585173d943
Pradhyumn Patidar <pradhyumn14patidar@gmail.com>
2024-07-13 13:08:09 +0530

    Configure database

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const requestListener = (req, res) => {
 };
 
 const server = http.createServer(requestListener);
-const PORT =  3000;
+const PORT = process.env.PORT ||  3000;
 sequelize.sync().then(() => {
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
